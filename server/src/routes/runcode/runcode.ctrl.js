951f47d7ef3958ffc9800b8e7a48c000bceeddb7
sunwookim <sunkim09@g.skku.edu>
2022-06-30 15:05:32 +0900

    ADD testcase api

--- server/src/routes/runcode/runcode.ctrl.js
+++ server/src/routes/runcode/runcode.ctrl.js
@@ -1,4 +1,5 @@
-const user = require("../../schema/UserSchema");
+const User = require("../../schema/UserSchema");
+const TestCase = require("../../schema/TestCaseSchema")
 const converter = require("./converter");
 const { runPython } = require("./runPython");
 const { Router } = require("express");
@@ -6,7 +7,7 @@ const router = Router();
 
 exports.get_root = async (req,res) => {
     try {
-        const users = await user.find({});
+        const users = await User.find({});
         res.send({ users: users });
     } catch (err) {
         console.log(err);
@@ -30,11 +31,22 @@ exports.post_run_code = async (req,res) => {
     }
 };
 
-// code 채점하고 테스슽 케이스 결과값 반환 
-// body : {code: string, input: {변수명: string, ...}} 
-exports.post_grade_code = async (req,res) => {
-    console.log(req.body);
+// code 채점하고 테스트 케이스 결과값 반환 
+// params: num, body : {code: string} 
+exports.put_grade_code = async (req,res) => {
+    // console.log(req.body);
 
+    // try {
+    //     const testcase = await TestCase.findOne({testnumber: req.params.num});
+        
+    //     // for testcase.input
+
+    //     // res.send({ users: users });
+    // } catch (err) {
+    //     console.log(err);
+    //     res.send("error");
+        
+    // }
 
 };
 
