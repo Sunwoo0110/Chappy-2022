4bf45e60dd36caced90bfc2dc7f5688859da7823
Hans-0101 <hyhsumin@naver.com>
2022-07-04 01:30:49 +0900

    feat:getHint from server success

--- cheppy/pages/codingPage/components/hint.tsx
+++ cheppy/pages/codingPage/components/hint.tsx
@@ -3,16 +3,50 @@ import { GET_USER, GET_USER_BY_USERID } from '../../../database/constants'
 import { DataUsageTwoTone } from '@mui/icons-material';
 import Link from "next/link"
 import { generateKey } from 'crypto';
+import { GetServerSideProps } from 'next'
+import React, { useEffect, useState } from "react";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import axios from 'axios';
-
+import getHint from './getHint';
 
 const Hint = () =>{
+    const [hints, setHint] = useState<any[]>([]);
+    const [hintNum, setHintNum] = useState(0);
 
-    const hints = {"2":[{"Delete":"this statement"}, {"Insert":"'if' statement"}], "3":[{"Insert":"'Return' statement"}]};    
-    const lines = Object.keys(hints);
+    useEffect(() => {
+        getHintData();
+        // if(Object.keys(hints).length!=0){
+        //     for(let i=0; i<Object.keys(hints).length; i++){
+        //         for(let j=0; j<hints[Object.keys(hints)[i]].length; i++){
+        //             // console.log(res.data[Object.keys(res.data)[i]].length);
+        //             setHintNum(hintNum+1);
+        //         }
+        //     }
+        // }
+    }, [])
+    
+    const getHintData = async () => {
+        await axios.post('http://localhost:4000/feedback/get_feedback', {
+            //feedback api 완성되면 연결
+        })
+        .then((res) => {
+            console.log("getHintData success");
+            console.log(res.data);
+            setHint(res.data);
+            const lines = Object.keys(res.data);
+            // for(let i=0; lines.length; i++){
+            //     for(let j=0; Object.keys(res.data[lines[i]]).length; j++){
+            //         setHintNum(hintNum+1);
+            //     }
+            // }
+    })
+        .catch(error => {
+            console.log("getHintData failed");
+            console.log(error.response);
+        })
+    }
 
     return(
         <>
@@ -24,59 +58,40 @@ const Hint = () =>{
                         </Grid>
                         <Grid item width="8%">
                             <Box sx={{ backgroundColor: "#FFD600", borderRadius: 2}}>
-                                <Typography fontWeight='bold' fontSize={13}  align="center">+5</Typography>
+                                <Typography fontWeight='bold' fontSize={13}  align="center">+{hintNum}</Typography>
                             </Box>
                         </Grid>
                     </Grid>
                 </Box>
 
-                {lines.map((line) => (
-                    hints[line].map((contents) => (
-                        Object.keys(contents).map((content) => (
-                            <List key={line.toString()+content.toString()} disablePadding>
-                                <ListItem>
-                                    <Grid container sx={{ml:3, mt:2}}>
-                                        <Grid item width="15%">
-                                            <Box sx={{backgroundColor: "#FFD600", borderRadius: 1}}>
+                {hints.length != 0 &&
+                    Object.keys(hints).map((line) => (
+                        hints[line].map((contents) => (
+                            Object.keys(contents).map((content) => (
+                                <List key={line.toString()+content.toString()} disablePadding>
+                                    <ListItem>
+                                        <Grid container sx={{ml:3, mt:2}}>
+                                            <Grid item width="15%">
+                                                <Box sx={{backgroundColor: "#FFD600", borderRadius: 1}}>
+                                                    <ListItemText>
+                                                        <Typography align="center" fontWeight='bold' fontSize={15}>line {line}</Typography>
+                                                    </ListItemText> 
+                                                </Box>
+                                            </Grid>
+                                            <Grid item width="75%">
                                                 <ListItemText>
-                                                    <Typography align="center" fontWeight='bold' fontSize={15}>line {line}</Typography>
-                                                </ListItemText> 
-                                            </Box>
+                                                    <Typography fontSize={13} style={{ marginLeft: "5%" }}>{content+" "+contents[content]}</Typography>
+                                                </ListItemText>
+                                            </Grid>
                                         </Grid>
-                                        <Grid item width="75%">
-                                            <ListItemText>
-                                                <Typography fontSize={13} style={{ marginLeft: "5%" }}>{content+" "+contents[content]}</Typography>
-                                            </ListItemText>
-                                        </Grid>
-                                    </Grid>
-                                </ListItem>
-                                </List>                                
+                                    </ListItem>
+                                    </List>                                
+                            ))
                         ))
                     ))
-                ))}
+                }
 
-                {/* {lines.map((line) => (
-                    <List key={line.toString()} disablePadding>
-                        <ListItem>
-                            <Grid container sx={{ml:3, mt:2}}>
-                                <Grid item width="15%">
-                                    <Box sx={{backgroundColor: "#FFD600", borderRadius: 1}}>
-                                        <ListItemText>
-                                            <Typography align="center" fontWeight='bold' fontSize={15}>{line}</Typography>
-                                        </ListItemText> 
-                                    </Box>
-                                </Grid>
-                                <Grid item width="75%">
-                                    <ListItemText>
-                                        <Typography fontSize={13} style={{ marginLeft: "5%" }}>Replace Return value</Typography>
-                                    </ListItemText>
-                                </Grid>
-                            </Grid>
-                        </ListItem>
-                    </List>
-                ))} */}
-
-                <Grid container sx={{ml:3, mt:2}}>
+                {/* <Grid container sx={{ml:3, mt:2}}>
                     <Grid item width="15%">
                         <Box sx={{backgroundColor: "#FFD600", borderRadius: 1}}>
                             <Typography align="center" fontWeight='bold' fontSize={15}>line 2</Typography>
@@ -118,7 +133,7 @@ const Hint = () =>{
                     <Grid item width="75%">
                         <Typography fontSize={13} style={{ marginLeft: "5%" }}>Insert If statement</Typography>
                     </Grid>
-                </Grid>
+                </Grid> */}
 
             </Box>
         </>
