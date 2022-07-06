8eba6a0faca997162a4b95f95e4dc407adeda7e6
Hans-0101 <hyhsumin@naver.com>
2022-07-06 14:02:11 +0900

    feat:making hint endpoint

---
+++ server/src/routes/hint/hint.ctrl.js
@@ -0,0 +1,40 @@
+// const connectDb = require("../db");
+const { Router } = require("express");
+const router = Router();
+
+// get hint
+exports.get_hint = async (req, res) => {
+    try{
+        let dummyHint = {
+            "2":[
+                {
+                  "Delete":"this statement"
+                }, 
+                {	
+                  "Insert":"'If' statement"
+                }
+                ],
+            "3":[
+                {
+                  "Insert":"'Return' statement"
+                }
+                ],
+            "4":[
+                {
+                    "Insert":"'else' statement"
+                }
+                ],    
+            "5":[
+                {
+                    "Insert":"'Assignment' statement"
+                }
+                ]        
+        };
+        console.log(req.body);     
+        res.send(dummyHint);      
+    } catch (err) {
+        console.log("error!!");
+        console.log(err);
+        res.send("error");        
+    }
+}
