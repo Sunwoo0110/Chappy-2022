6de3eaaced5ad3ebf444f0de98fabef6cf30f270
bwj2800 <bwj2800@naver.com>
2022-07-07 02:37:07 +0900

    copyClipboard() added

--- cheppy/pages/codingPage/components/testcase.tsx
+++ cheppy/pages/codingPage/components/testcase.tsx
@@ -5,8 +5,12 @@ import * as React from 'react';
 
 const Testcase = () =>{
     const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+    //testcase 버튼 클릭시 클립보드 복사
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        // console.log(event.currentTarget)
         setAnchorEl(event.currentTarget);
+        copyClipboard("main([1, 1, 1, 2, 3])");
     };
     
     const handleClose = () => {
@@ -16,6 +20,20 @@ const Testcase = () =>{
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
+    //클립보드 복사 함수
+    const copyClipboard = async (
+        text: string,
+        successAction?: () => void,
+        failAction?: () => void,
+        ) => {
+        try {
+            await navigator.clipboard.writeText(text);
+            successAction && successAction();
+        } catch (error) {
+            failAction && failAction();
+        }
+    };
+
     return(
         <>
             <Box sx={{width: '25vw'}} >
@@ -23,19 +41,21 @@ const Testcase = () =>{
                     <Typography fontWeight='bold' fontSize={15} style={{ marginLeft: "3%", color: 'white' }}>테스트케이스</Typography>
                 </Box>
 
-                <Button variant="outlined" style={{ marginLeft: "3%", marginTop:15 }} onClick={handleClick}>테스트케이스 1번</Button>
-                <Popover id={id} open={open} anchorEl={anchorEl} onClose={handleClose} anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}>
-                    <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
-                </Popover>
-                <Typography fontSize={13} style={{ marginLeft: "3%" }}>입력값: main([1, 1, 1, 2, 3])</Typography>
-                <Typography fontSize={13} style={{ marginLeft: "3%" }}>출력값: [1, 2, 3]</Typography>
+                <Box sx={{height:'50vh', overflow: 'scroll'}}>
+                    <Button variant="outlined" style={{ marginLeft: "3%", marginTop:15 }} onClick={handleClick}>테스트케이스 1번</Button>
+                    <Popover id={id} open={open} anchorEl={anchorEl} onClose={handleClose} anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}>
+                        <Typography sx={{ p: 2 }}>main([testcase]) copied.</Typography>
+                    </Popover>
+                    <Typography fontSize={13} style={{ marginLeft: "3%" }}>입력값: main([1, 1, 1, 2, 3])</Typography>
+                    <Typography fontSize={13} style={{ marginLeft: "3%" }}>출력값: [1, 2, 3]</Typography>
 
-                <Button variant="outlined" style={{ marginLeft: "3%" , marginTop:15}} onClick={handleClick}>테스트케이스 2번</Button>
-                <Popover id={id} open={open} anchorEl={anchorEl} onClose={handleClose} anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}>
-                    <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
-                </Popover>
-                <Typography fontSize={13} style={{ marginLeft: "3%" }}>입력값: main([3, 4, 5, 1, 2])</Typography>
-                <Typography fontSize={13} style={{ marginLeft: "3%" }}>출력값: [3, 4, 5, 1]</Typography>
+                    <Button variant="outlined" style={{ marginLeft: "3%" , marginTop:15}} onClick={handleClick}>테스트케이스 2번</Button>
+                    <Popover id={id} open={open} anchorEl={anchorEl} onClose={handleClose} anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}>
+                        <Typography sx={{ p: 2 }}>main([testcase]) copied.</Typography>
+                    </Popover>
+                    <Typography fontSize={13} style={{ marginLeft: "3%" }}>입력값: main([3, 4, 5, 1, 2])</Typography>
+                    <Typography fontSize={13} style={{ marginLeft: "3%" }}>출력값: [3, 4, 5, 1]</Typography>
+                </Box>
             </Box>
         </>
     )
