3d6be7978eb157ef70df97a41acb860d802bc671
Hans-0101 <hyhsumin@naver.com>
2022-07-15 11:43:09 +0900

    feat:insert contents in scroll view

--- pages/assignment/components/_rightsidebar.js
+++ pages/assignment/components/_rightsidebar.js
@@ -33,27 +33,26 @@ function Hint() {
 
         <div className={styles.feedback}>
             <h3 className={styles.section_title}>힌트</h3>
-            <div style={{overflowY: "scroll", height:"100%"}}></div>
+            <div style={{overflowY: "scroll", height:"100%"}}>
+                {hintVal.content == null &&
+                    <div>Loading ... </div>
+                }
 
-            {hintVal.content == null &&
-                <div>Loading ... </div>
-            }
+                {hintVal.num == -1 &&
+                    <div>{hintVal.content} </div>
+                }
 
-            {hintVal.num == -1 &&
-                <div>{hintVal.content} </div>
-            }
-
-            {hintVal.num!=-1 && hintVal.content!=null &&
-                Object.keys(hintVal.content).map((line) => (
-                    hintVal.content[line].map((contents) => (
-                        Object.keys(contents).map((content) => (
-                            <li key={line.toString()+content.toString()}>
-                                <div>line {line}</div>
-                                <div>{content+" "+contents[content]}</div>
-                            </li>
-                ))))))
-            }
-
+                {hintVal.num!=-1 && hintVal.content!=null &&
+                    Object.keys(hintVal.content).map((line) => (
+                        hintVal.content[line].map((contents) => (
+                            Object.keys(contents).map((content) => (
+                                <li key={line.toString()+content.toString()}>
+                                    <div>line {line}</div>
+                                    <div>{content+" "+contents[content]}</div>
+                                </li>
+                    ))))))
+                }
+            </div>
         </div>
     )
 }
@@ -78,21 +77,21 @@ function Feedbacks(props) {
         <div className={styles.feedback}>
             <div className={styles.section_title}>해결방안</div>
             <div style={{overflowY: "scroll", height:"100%"}}>
-            <ul>
-                {testcases.map((tc) => {
-                    if (!tc.is_open) return
-                    return (
-                        <li key={testcases.indexOf(tc)}>
-                            <div className={styles.content}>
-                                {tc.inputs}: {tc.expected_output}
-                                <button style={{marginLeft:"15px"}} type="button" class="btn btn-outline-primary" 
-                                onClick={() => navigator.clipboard.writeText(`main(${tc.inputs})`)}>Copy</button>
-                            </div>
-                        </li>
-                    )
-                }
-                )}
-            </ul>
+                <ul>
+                    {testcases.map((tc) => {
+                        if (!tc.is_open) return
+                        return (
+                            <li key={testcases.indexOf(tc)}>
+                                <div className={styles.content}>
+                                    {tc.inputs}: {tc.expected_output}
+                                    <button style={{marginLeft:"15px"}} type="button" class="btn btn-outline-primary" 
+                                    onClick={() => navigator.clipboard.writeText(`main(${tc.inputs})`)}>Copy</button>
+                                </div>
+                            </li>
+                        )
+                    }
+                    )}
+                </ul>
             </div>
         </div>
     )
@@ -101,7 +100,7 @@ function Feedbacks(props) {
 //여기서도 피드백이 어떻게 진행될지 정해지지 않아 임의의 값으로 비교
 function CompareAnswer() {
     // style needs to be fixed
-    const newStyles = {
+    const diffStyle = {
         variables: {
           light: {
             codeFoldGutterBackground: "#6F767E",
@@ -113,18 +112,18 @@ function CompareAnswer() {
     return (
         <div className={styles.feedback}>
             <h3 className={styles.section_title}>답안비교</h3>
-            <div style={{overflowY: "scroll", height:"100%"}}></div>
-
-            <ReactDiffViewer
-                    oldValue="print('hello')"
-                    newValue="print('hihi)"
-                    splitView={false}
-                    compareMethod={DiffMethod.LINES}
-                    styles={newStyles}
-                    // leftTitle="Version A"
-                    // rightTitle="Version B"
-                    // renderContent={highlightSyntax}
-            />
+            <div style={{overflowY: "scroll", height:"100%"}}>
+                <ReactDiffViewer
+                        oldValue="print('hello')"
+                        newValue="print('hihi)"
+                        splitView={false}
+                        compareMethod={DiffMethod.LINES}
+                        styles={diffStyle}
+                        // leftTitle="Version A"
+                        // rightTitle="Version B"
+                        // renderContent={highlightSyntax}
+                />
+            </div>
         </div>
     )
 }
