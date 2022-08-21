3c71795a04f92995dfba18963f94533bccf65b56
bwj2800 <bwj2800@naver.com>
2022-08-21 14:43:17 +0900

    MERGE #3 #5

--- pages/lectureDetail/components/_footer.js
+++ pages/lectureDetail/components/_footer.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../../../styles/lectureDetail/lectureDetail.module.css";
+import styles from "../../../styles/lectureDetail/LectureDetail.module.css";
 import {Envelope, Instagram, TelephoneFill} from 'react-bootstrap-icons';
 
 export default function Footer() {
