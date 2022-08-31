afa85fd8955cb2d8a5a338e926fe1b1ec7064c7d
bwj2800 <bwj2800@naver.com>
2022-09-01 01:18:55 +0900

    FEAT header and footer separated

--- pages/lecture/searchlecture.js
+++ pages/lecture/searchlecture.js
@@ -1,7 +1,7 @@
 import {useState } from "react";
 
-import Header from "./components/_header";
-import Footer from "./components/_footer";
+import Header from "../components/_header";
+import Footer from "../components/_footer";
 
 import styles from "../../styles/lecture/Lecture.module.css";
 
