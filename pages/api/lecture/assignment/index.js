b0578abfb36ba0fa3f8c520f37b8cc570209ec96
bwj2800 <bwj2800@naver.com>
2022-10-06 04:53:26 +0900

    DELETE console.log

--- pages/api/lecture/assignment/index.js
+++ pages/api/lecture/assignment/index.js
@@ -11,8 +11,6 @@ export default async function handler(req, res) {
         case 'GET':
             try {
                 let query = qs.parse(req.query);
-                console.log("=================================\n", req.query);
-                console.log("=================================\n", query);
                 const assignments = await Assignment.find(query);
                 res.status(200).json({ success: true, data: assignments });
             } catch (error) {
