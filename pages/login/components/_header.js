47c8e42e900826ad4ccfac65997cdfde5e5ba395
bwj2800 <bwj2800@naver.com>
2022-10-19 11:35:40 +0900

    ADD link to main page

--- pages/login/components/_header.js
+++ pages/login/components/_header.js
@@ -5,10 +5,12 @@ import { IoIosArrowBack } from "react-icons/io";
 export default function Header() {
     return (
         <div className={commonStyles.header}>
+            <Link href="/">
             <div className={headerStyles["back-txt"]}>
                 <IoIosArrowBack size="35px"/>
                 메인으로 돌아가기
             </div>
+            </Link>
             <div className={headerStyles["login-txt"]}>로그인</div>
             <div className={headerStyles["login-desc-txt"]}>로그인하고 당신의 코딩 생활을 시작하세요</div>
         </div>
