e6676e575bb4da51efa2541169ac823d5d289972
bwj2800 <bwj2800@naver.com>
2022-10-20 11:41:14 +0900

    ADD findpw page

---
+++ pages/login/components/_findHeader.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+import commonStyles from "../../../styles/login/Login.module.css"
+import headerStyles from "../../../styles/login/_header.module.css"
+import { IoIosArrowBack } from "react-icons/io";
+
+export default function Header() {
+    return (
+        <div className={commonStyles.header}>
+            <Link href="/">
+            <div className={headerStyles["back-txt"]}>
+                <IoIosArrowBack size="35px"/>
+                메인으로 돌아가기
+            </div>
+            </Link>
+            <div className={headerStyles["login-txt"]}>비밀번호 찾기</div>
+            <div className={headerStyles["login-desc-txt"]}>로그인하고 당신의 코딩 생활을 시작하세요</div>
+        </div>
+    )
+}
---
+++ pages/login/components/_findpw.js
@@ -0,0 +1,89 @@
+import Link from "next/link";
+
+import { useCallback, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import commonStyles from "../../../styles/login/Login.module.css"
+import loginStyles from "../../../styles/login/_login.module.css"
+import * as userActions from "../../../store/modules/user";
+
+export default function Login() {
+    const dispatch = useDispatch();
+    const userId = useSelector(state => state.user);
+
+    const [inputs, setInputs] = useState({
+        id: '',
+        pwd: '',
+    });
+
+    const {id, pwd} = inputs;
+
+    const onChangeInputs = (e) => {
+       const {name, value} = e.target;
+       setInputs({
+            ...inputs,
+            [name]:value,
+       });
+    };
+
+    const setUserId = useCallback( (user_id) => {
+        let payload = {
+            id: user_id,
+        };
+        dispatch(userActions.setUser(payload));
+    }, [dispatch]);
+
+    async function onLogin() {
+        console.log(userId);
+        await fetch('/api/user/profile/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify({
+                "id": id,
+                "pwd": pwd,    
+            }),
+        })
+        .then(response => response.json())
+        .then(response => {
+            // console.log(response);
+            //로그인 성공
+            if(response.data!=-1){
+                setUserId(response.data);
+                window.location.href = "/lecture";
+            }
+        })        
+        .catch(function(err) {
+            console.log(err);
+        })
+    };
+
+    return (
+        <div className={commonStyles.main}>
+            <div className={loginStyles["input-content"]}>
+                <div className={loginStyles.statement}>ID</div>
+                <input 
+                    name="id" 
+                    className={loginStyles.input}
+                    onChange={onChangeInputs}
+                    value={id}
+                />
+                <div className={loginStyles.statement}>이메일</div>
+                <input 
+                    name="pwd" 
+                    className={loginStyles.input}
+                    onChange={onChangeInputs}
+                    value={pwd}
+                    type={"password"}
+                />
+            </div>
+            <div className={loginStyles["login-btn"]}>비밀번호 찾기</div>
+            <div className={loginStyles["join-content"]}>
+                <div className={loginStyles.desc}>아직 회원이 아니신가요?</div>
+                <Link href="/signup">
+                <div className={loginStyles["join-txt"]}>회원가입하기</div>
+                </Link>
+            </div>
+        </div>
+    );
+}
